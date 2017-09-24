90213b12a8bbd62e57a8261286e215186b21f75b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-24 08:27:00 -0400

    Update HighFrequencyFinance.jl

--- src/HighFrequencyFinance.jl
+++ src/HighFrequencyFinance.jl
@@ -1,7 +1,11 @@
 module HighFrequencyFinance
 
+# maxmin, minmidmax, maxmidmin
+include("ordering/minmidmax.jl")
+
 # very fast median of 1,2,..,15,16 values
 include("onlinestats/smallstats/median_1to16.jl")  
+
 # fast rolling of windowed function over a vector
 include("dataflow/running.jl")
 
