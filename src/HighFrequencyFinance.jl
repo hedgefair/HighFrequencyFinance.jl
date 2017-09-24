7f29eff8b80f564ef68031088159ce7edbb9d9ff
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-24 08:15:52 -0400

    Update HighFrequencyFinance.jl

--- src/HighFrequencyFinance.jl
+++ src/HighFrequencyFinance.jl
@@ -1,5 +1,6 @@
 module HighFrequencyFinance
 
-include("dataflow/running.jl")  # roll a windowed function over a value vector
+include("onlinestats/median_1to16.jl")  # very fast median of 1,2,..,15,16 values
+include("dataflow/running.jl")          # fast rolling of windowed function over a vector
 
 end # module
