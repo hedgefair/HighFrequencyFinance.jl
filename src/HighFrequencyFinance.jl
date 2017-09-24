b1263d77f391cae044b85dcfede1f4eec3215d52
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-24 08:29:21 -0400

    Update HighFrequencyFinance.jl

--- src/HighFrequencyFinance.jl
+++ src/HighFrequencyFinance.jl
@@ -2,6 +2,8 @@ module HighFrequencyFinance
 
 # maxmin, minmidmax, maxmidmin
 include("ordering/minmidmax.jl")
+# disentanglement optimal sequency maps
+include("ordering/sequency_3to8.jl")
 
 # very fast median of 1,2,..,15,16 values
 include("onlinestats/smallstats/median_1to16.jl")  
