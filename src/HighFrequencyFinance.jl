ec67cf9ab0f5e56c41e73494d90ba8bde2eb45f2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-09-24 23:57:50 -0400

    Update HighFrequencyFinance.jl

--- src/HighFrequencyFinance.jl
+++ src/HighFrequencyFinance.jl
@@ -1,3 +1,37 @@
+#=>
+
+maxmin(a,b), minmidmax(a,b,c), maxmidmin(a,b,c)
+
+median(x_1) .. median(x_1 .. x_16)
+
+sequency(a) .. sequency(a,b,c,d,e,f,g,h)
+sequency_rev(a) .. sequency_rev(a,b,c,d,e,f,g,h)
+sequency_indicies(1..8)
+sequency_indicies_rev(1..8)
+order_rev(a) .. order_rev(a,b,c,d,e,f,g,h)
+
+     running windowed functions over a data vector
+    
+   - rolling(fn, span, data)
+     -- length(result) == length(data) - span + 1
+     -- all result values use fn( window-sized data )
+   
+   - rolling_ahead(fn, span, data)
+     -- length(result) == length(data)
+     -- result[1:span-1] use fn(coarser data)      
+   
+   - rolling_behind(fn, span, data)
+      -- length(result) == length(data)    
+      --  result[end-span+1:end] use fn(coarser data) 
+   - rolling_first(fn, span, data)
+     -- length(result) == length(data)    
+     -- result[end-span+1:end] == data[end-span+1:end]
+   - rolling_final(fn, span, data)
+     -- length(result) == length(data)    
+     -- result[1:span-1] == data[1:span-1]
+
+<=#
+
 module HighFrequencyFinance
 
 # maxmin, minmidmax, maxmidmin
